0665a2bf6120880d197f831f1551a9b8aed2157d
ADOLFO BALSEIRO <abalseiro@hotmail.com>
2022-11-28 19:48:56 +0000

    BBDD finalizada

--- src/models.py
+++ src/models.py
@@ -8,23 +8,40 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class user(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(20), nullable=False)
+    surname = Column(String(25))
+    email = Column(String(30), nullable=False)
+    password = Column(String(25), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favourites_Planets(Base):
+    __tablename__ = 'favourites_planets'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planets_id = Column(Integer, ForeignKey('planets.id'))
+
+class Favourites_Characters(Base):
+    __tablename__ = 'favourites_characters'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    
+
+class planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(20), nullable=False)
+    details = Column(String(200), nullable=False)
+    image = Column(String(25), nullable=False)
+
+class characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String(20), nullable=False)
+    details = Column(String(200), nullable=False)
+    image = Column(String(25), nullable=False)
 
     def to_dict(self):
         return {}
